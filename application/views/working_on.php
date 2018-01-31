bf06c6b298754126ebc827af4f503f4926890d3f
d-silvas <d.silvas@outlook.com>
2018-01-31 09:54:34 +0000

    Finish portfast

--- application/views/working_on.php
+++ application/views/working_on.php
@@ -2,7 +2,7 @@
 </style>
 <!--<img src="../../../assets/images/00setup.png" alt="Setup">-->
 <h4 class="subtitle">Setup</h4>
-<!--<img src="../../../assets/images/0005.png" alt="Setup">-->
+<img src="../../../assets/images/0008.png" alt="Setup">
 <br>
 <br>
 <h4 class="subtitle">Objectives</h4>
@@ -18,12 +18,8 @@
     BPDUGuard.
   </li>
   <li>
-    Ensure BPDUGuard is working correctly - connect Mini-Me to the network
-    and
-  </li>
-  <li>
-    Have Belt advertise the default route to the other routes via OSPF.
-    The route should exist even if Belt does not have a default route.
+    Ensure BPDUGuard is working correctly - connect switch Mini-Me to the
+    network and ensure appropiate action is taken.
   </li>
 </ol>
 <br>
@@ -32,15 +28,14 @@
 <ul><li>
   Let's make sure Dr Evil is the root bridge first
 </li></ul>
-<pre class="terminal">DrEvil(config)# <b>spanning-tree vlan 1 root primary
+<pre class="terminal">DrEvil(config)# <b>spanning-tree vlan 1 root primary</b>
 </pre>
 <ul><ul><li>
   This command tells the switch "look around, see what else is out there,
   and set your priority lower than everything else".
 </li></ul></ul>
-<br>
-<pre class="terminal">DrEvil(config)#int range fa1/0 - 15
-DrEvil(config-if-range)#spanning-tree portfast
+<pre class="terminal">DrEvil(config)# <b>int range fa1/0 - 15</b>
+DrEvil(config-if-range)# <b>spanning-tree portfast</b>
 %Warning: portfast should only be enabled on ports connected to a single host.
  Connecting hubs, concentrators, switches,  bridges, etc.to this interface
  when portfast is enabled, can cause temporary spanning tree loops.
@@ -51,3 +46,44 @@ DrEvil(config-if-range)#spanning-tree portfast
 %Portfast has been configured on FastEthernet1/15 but will only
  have effect when the interface is in a non-trunking mode.
 </pre>
+<ul><ul><li>
+  For security reasons, the portfast config will only work when the interface
+  is connected to an end device (it will shutdown in case it receives attempts
+  of trunking negotiation)
+</li></ul></ul>
+<br>
+<h4><b>[3]</b> BPDUGuard configuration</h4>
+<hr>
+<ul>
+  <li>
+    On some switches we can activate it like this:
+  </li>
+</ul>
+<pre class="terminal">DrEvil(config)# <b>int range fa1/0 - 15</b>
+DrEvil(config-if-range)# <b>spanning-tree portfast bpduguard</b>
+</pre>
+<br>
+<ul>
+  <li>
+    On some other versions:
+  </li>
+</ul>
+<pre class="terminal">DrEvil(config)# <b>int range fa1/0 - 15</b>
+DrEvil(config-if-range)# <b>spanning-tree bpduguard enable</b>
+</pre>
+<br>
+<ul>
+  <li>
+    When the BPDUGuard is activated, the switch thinks "if I see a BPDU coming
+    in, I'm going to turn off the interface"
+  </li>
+  <li>
+    Interface will go in an <b>err-disable</b> status. We can check this in
+    <b>show interfaces fa1/0</b>.
+  </li>
+  <li>
+    We would need to do <b>shutdown</b> + <b>no shutdown</b> on that interface.
+  <li>
+    In GNS3, BPDUGuard doesn't work, so no examples.
+  </li>
+</ul>
